e6b9733cff6eee2399c2540c7de1394e15e2fef9
jetmaquiling <jet.maquiling@gmail.com>
2021-11-05 17:46:55 +0800

    RCK MOBILE

--- pages/v1/network.js
+++ pages/v1/network.js
@@ -7,25 +7,70 @@ import Link from 'next/link'
 import HeadV2 from '@/components/header/headv2'
 
 export default function Home() {
-  const addNet = () =>{
-    window.ethereum.request({
-      method: 'wallet_addEthereumChain',
-      params: [{
-      chainId: '0x38',
-      chainName: 'Binance Smart Chain',
-      nativeCurrency: {
-          name: 'Binance Coin',
-          symbol: 'BNB',
-          decimals: 18
-      },
-      rpcUrls: ['https://bsc-dataseed.binance.org/'],
-      blockExplorerUrls: ['https://bscscan.com']
-      }]
-      })
-      .catch((error) => {
+  const addNet = async () =>{
+    try {
+      const network = await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [{
+        chainId: '0x38',
+        chainName: 'Binance Smart Chain',
+        nativeCurrency: {
+            name: 'Binance Coin',
+            symbol: 'BNB',
+            decimals: 18
+        },
+        rpcUrls: ['https://bsc-dataseed.binance.org/'],
+        blockExplorerUrls: ['https://bscscan.com']
+        }]
+        }) 
+
+      if (network) {
+        confirm("Okay");
+      } else {
+        console.log('Error!');
+        confirm("It's already in Binance Smart Chain Network");
+      }
+    } catch (error) {
       console.log(error)
-      }) 
+      confirm("Something Went Wrong");
+    }
+      
   }
+
+  const addRCKTOKEN = async () =>{
+    console.log("working")
+    try {
+      const network = await window.ethereum.request({
+        method: 'wallet_addEthereumChain',
+        params: [{
+        chainId: '0x38',
+        chainName: 'Binance Smart Chain',
+        nativeCurrency: {
+            name: 'Binance Coin',
+            symbol: 'BNB',
+            decimals: 18
+        },
+        rpcUrls: ['https://bsc-dataseed.binance.org/'],
+        blockExplorerUrls: ['https://bscscan.com']
+        }]
+        }) 
+        console.log("working2")
+
+      if (network) {
+        console.log("working")
+        addRCK();
+        
+      } else {
+        addRCK();
+      }
+    } catch (error) {
+      console.log(error)
+      console.log("working")
+      confirm("Something Went Wrong");
+    }
+      
+  }
+
 
   const addRCK = async () =>{
     const tokenAddress = '0xF305B7eD52c5C45068e7E697c28cDECd13e8115b';
@@ -49,7 +94,7 @@ export default function Home() {
       });
 
       if (wasAdded) {
-        console.log('Thanks for your interest!');
+        console.log('GOOD REQUEST');
       } else {
         console.log('Error!');
       }
@@ -66,12 +111,12 @@ export default function Home() {
       <div className={style.section}>
         <HeadV2/>
         <div className={style.sectionError} >
-            <h3 style={{color: '#fff'}}>Add Binance Smart Chain Mainet </h3>
-            <div className={style.buttonBox} style={{ margin: '20px 0px'}}>
+            <h3 style={{color: '#fff'}}>SETUP YOUR WALLET</h3>
+            <div className={style.buttonBox} style={{ margin: '20px 0px', textAlign:'center'}}>
                 <h4 onClick={addNet} className={style.h4animate} style={{padding: "10px"}} >CONNECT TO BNB SMART CHAIN</h4>
             </div>
-            <div className={style.buttonBox} style={{ margin: '20px 0px'}}>
-                <h4 onClick={addRCK} className={style.h4animate} style={{padding: "10px"}} >ADD RCK TOKEN TO WALLET</h4>
+            <div className={style.buttonBox} style={{ margin: '0px 0px'}}>
+                <h4 onClick={addRCKTOKEN} className={style.h4animate} style={{padding: "10px", textAlign:'center'}} >ADD RCK TOKEN TO WALLET</h4>
             </div>
             
         </div>
